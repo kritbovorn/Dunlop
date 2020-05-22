035bdae33bec201310447cc2a1fa1e2fa8b3bd0d
kritbovorn taweeyossak <ktaweeyossak@yahoo.com>
2020-05-22 13:35:58 +0700

    Create Xcode project

---
+++ Dunlop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dunlop
+//
+//  Created by Kritbovorn on 22/5/2563 BE.
+//  Copyright © 2563 Kritbovorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
