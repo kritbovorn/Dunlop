f1c869f2b91092c06ffa6edc9065aa1079b78f65
kritbovorn taweeyossak <ktaweeyossak@yahoo.com>
2020-05-24 16:17:40 +0700

    Perfect and try change shadow background of product again

---
+++ Dunlop/Content/SubContent/CategoryTextView.swift
@@ -0,0 +1,55 @@
+//
+//  CategoryTextView.swift
+//  Dunlop
+//
+//  Created by Kritbovorn on 24/5/2563 BE.
+//  Copyright © 2563 Kritbovorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryTextView: View {
+    
+    var title: String
+    var contentsize: CGFloat
+    var fontSize: CGFloat
+    
+    var body: some View {
+        
+        
+        GeometryReader { mainGeo in
+            
+            VStack(spacing: 0) {
+                
+                GeometryReader { geo in
+                    
+                    VStack(spacing: 0) {
+                        
+                        Spacer()
+                        
+                        Text(self.title)
+                            .font(.system(size: self.fontSize, weight: .bold))
+                        .foregroundColor(.lairDarkGray)
+                        
+                        .shadow(color: Color.white.opacity(0.3), radius: geo.size.width * 0.004, x: -geo.size.width * 0.004, y: -geo.size.width * 0.004)
+                        .shadow(color: Color.lairShadowGray, radius: geo.size.width * 0.004, x: geo.size.width * 0.004, y: geo.size.width * 0.004)
+                        
+                        Spacer()
+                        
+                        
+                    }
+                    
+                    
+                }
+                .frame(height: mainGeo.size.height * self.contentsize)
+                
+            }
+        }
+    }
+}
+
+struct CategoryTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryTextView(title: "Boie", contentsize: 0.1, fontSize: 24)
+    }
+}
--- Dunlop/View/HomeView.swift
+++ Dunlop/View/HomeView.swift
@@ -24,28 +24,57 @@ struct HomeView: View {
                     ScrollView(.vertical, showsIndicators: true) {
                         
                         
-                        VStack(alignment: .center, spacing: 0) {
+                        VStack {
                             
-                            // FIXME: - FIRST
+                            // TODO: - First Geometry
+                            
+                            // FIXME: - FIRST  Image Logo
                             GeometryReader { geo in
                                     
-                                Image("dunlop-logo")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: geo.size.width * 0.5)
+                                VStack(spacing: 0) {
+                                    
+                                    Spacer()
+                                    
+                                    HStack {
+                                        
+                                        Spacer()
+                                        
+                                        Image("dunlop-logo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: geo.size.width * 0.5)
+                                        
+                                        Spacer()
+                                        
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    
+                                }
                                 
                             }
                             .frame(height: mainGeo.size.height * 0.1)
                             
-                            // FIXME: - SECOND
+                    // FIXME: - Category Text 1
+                            
+                            GeometryReader { categoryOne in
+                                
+                                
+                                CategoryTextView(title: "Passenger Car", contentsize: 1, fontSize: 18)
+                                    .padding(.leading, 24)
+                                
+                                
+                            }
+                            .frame(height: mainGeo.size.height * 0.1)
+                            
+                            
+                            // TODO: - Product 1
                             GeometryReader { firstGeo in
                                 
                                 VStack(alignment: .leading, spacing: 0) {
                                     
-                                    Text("Passenger Car")
-                                        .font(.system(size: 14))
-                                        .fontWeight(.bold)
-                                        .padding(.leading, 20)
+                                    
                                     
                                     ProductRowView(resizeContent: 0.9, items: [
                                         ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm705"), title: "SP SPORT LM 705"),
@@ -61,61 +90,23 @@ struct HomeView: View {
                             .frame(height: mainGeo.size.height * 0.4)
                             
                             
-                            // FIXME: - SECOND
-                            GeometryReader { firstGeo in
+            // FIXME: - Category Text 2
+                    
+                            GeometryReader { categoryOne in
                                 
-                                VStack(alignment: .leading, spacing: 0) {
-                                    
-                                    Text("Van / Pick up")
-                                        .font(.system(size: 14))
-                                        .fontWeight(.bold)
-                                        .padding(.leading, 20)
-                                    
-                                    ProductRowView(resizeContent: 0.9, items: [
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm705"), title: "SP SPORT LM 705"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm704"), title: "SP SPORT LM 704"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("Maxx050"), title: "SP SPORT MAXX 050+"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300"), title: "ENASAVE EC 300"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300+"), title: "ENASAVE EC 300+"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("touring"), title: "SP TOURING R1")
-                                    ])
-                                }
+                                CategoryTextView(title: "Van/Pick up", contentsize: 1, fontSize: 18)
+                                .padding(.leading, 24)
                                 
                             }
-                            .frame(height: mainGeo.size.height * 0.4)
+                            .frame(height: mainGeo.size.height * 0.1)
                             
-                            // FIXME: - SECOND
+                            
+                            // TODO: - Product 2
                             GeometryReader { firstGeo in
                                 
                                 VStack(alignment: .leading, spacing: 0) {
                                     
-                                    Text("Van / Pick up")
-                                        .font(.system(size: 14))
-                                        .fontWeight(.bold)
-                                        .padding(.leading, 20)
                                     
-                                    ProductRowView(resizeContent: 0.9, items: [
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm705"), title: "SP SPORT LM 705"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm704"), title: "SP SPORT LM 704"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("Maxx050"), title: "SP SPORT MAXX 050+"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300"), title: "ENASAVE EC 300"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300+"), title: "ENASAVE EC 300+"),
-                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("touring"), title: "SP TOURING R1")
-                                    ])
-                                }
-                                
-                            }
-                            .frame(height: mainGeo.size.height * 0.4)
-                            
-                            // FIXME: - SECOND
-                            GeometryReader { firstGeo in
-                                
-                                VStack(alignment: .leading, spacing: 0) {
-                                    
-                                    Text("Van / Pick up")
-                                        .font(.system(size: 14))
-                                        .fontWeight(.bold)
-                                        .padding(.leading, 20)
                                     
                                     ProductRowView(resizeContent: 0.9, items: [
                                         ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm705"), title: "SP SPORT LM 705"),
@@ -131,7 +122,69 @@ struct HomeView: View {
                             .frame(height: mainGeo.size.height * 0.4)
                             
                             
+                // FIXME: - Category Text 3
                             
+                                    GeometryReader { categoryOne in
+                                        
+                                       CategoryTextView(title: "4x4/SUV", contentsize: 1, fontSize: 18)
+                                        .padding(.leading, 24)
+                                        
+                                    }
+                                    .frame(height: mainGeo.size.height * 0.1)
+                            
+                            // TODO: - Product 3
+                            GeometryReader { firstGeo in
+                                
+                                VStack(alignment: .leading, spacing: 0) {
+                                    
+                                    
+                                    
+                                    ProductRowView(resizeContent: 0.9, items: [
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm705"), title: "SP SPORT LM 705"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm704"), title: "SP SPORT LM 704"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("Maxx050"), title: "SP SPORT MAXX 050+"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300"), title: "ENASAVE EC 300"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300+"), title: "ENASAVE EC 300+"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("touring"), title: "SP TOURING R1")
+                                    ])
+                                }
+                                
+                            }
+                            .frame(height: mainGeo.size.height * 0.4)
+                            
+                            
+                // FIXME: - Category Text 4
+                            
+                                    GeometryReader { categoryOne in
+                                        
+                                        CategoryTextView(title: "OEM", contentsize: 1, fontSize: 18)
+                                        .padding(.leading, 24)
+                                        
+                                    }
+                                    .frame(height: mainGeo.size.height * 0.1)
+                            
+                            
+                            
+                            
+                            // TODO: - Product 4
+                            GeometryReader { firstGeo in
+                                
+                                VStack(alignment: .leading, spacing: 0) {
+                                    
+                                    
+                                    
+                                    ProductRowView(resizeContent: 0.9, items: [
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm705"), title: "SP SPORT LM 705"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("lm704"), title: "SP SPORT LM 704"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("Maxx050"), title: "SP SPORT MAXX 050+"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300"), title: "ENASAVE EC 300"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("ec300+"), title: "ENASAVE EC 300+"),
+                                        ProductView(resizeContent: 1, fillColor: Color(red: 255/255, green: 212/255, blue: 0/255), imageName: Image("touring"), title: "SP TOURING R1")
+                                    ])
+                                }
+                                
+                            }
+                            .frame(height: mainGeo.size.height * 0.4)
                             
                             
                             
