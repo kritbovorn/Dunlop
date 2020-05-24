556ef7d64fa6246e96b4f05f3b4c12a5ef82c744
kritbovorn taweeyossak <ktaweeyossak@yahoo.com>
2020-05-24 19:13:16 +0700

    Try to layout HomeView reduce code

--- Dunlop/View/HomeView.swift
+++ Dunlop/View/HomeView.swift
@@ -11,157 +11,176 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         
-        // FIXME: - MAIN
-        GeometryReader { mainGeo in
+        
+        ZStack {
             
+            Color(red: 255/255, green: 222/255, blue: 0/255)
+                .edgesIgnoringSafeArea(.all)
+            
+            // FIXME: - MAIN
+            GeometryReader { mainGeo in
                 
-                ZStack {
                     
+                VStack(spacing: 0) {
                     
-                    Color(red: 255/255, green: 222/255, blue: 0/255)
-                        .edgesIgnoringSafeArea(.all)
-                    
-                    ScrollView(.vertical, showsIndicators: true) {
+                    GeometryReader { oneGeo in
                         
                         
-                        VStack {
-                            
-                            // TODO: - First Geometry
-                            
-                            // FIXME: - FIRST  Image Logo
-                            GeometryReader { geo in
+                        
+                        ZStack {
+                                
+                                Color(red: 255/255, green: 222/255, blue: 0/255)
+                                    .edgesIgnoringSafeArea(.all)
+                                
+                                ScrollView(.vertical, showsIndicators: true) {
                                     
-                                VStack(spacing: 0) {
                                     
-                                    Spacer()
-                                    
-                                    HStack {
+                                    VStack {
                                         
-                                        Spacer()
+                                        // TODO: - First Geometry
                                         
-                                        Image("dunlop-logo")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: geo.size.width * 0.5)
+                                        // FIXME: - FIRST  Image Logo
+                                        GeometryReader { geo in
+                                                
+                                            VStack(spacing: 0) {
+                                                
+                                                Spacer()
+                                                
+                                                HStack {
+                                                    
+                                                    Spacer()
+                                                    
+                                                    Image("dunlop-logo")
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(width: geo.size.width * 0.5)
+                                                    
+                                                    Spacer()
+                                                    
+                                                }
+                                                
+                                                Spacer()
+                                                
+                                                
+                                            }
+                                            
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.1)
                                         
-                                        Spacer()
+                                // FIXME: - Category Text 1
+                                        
+                                        GeometryReader { categoryOne in
+                                            
+                                            
+                                            CategoryTextView(title: "Passenger Car", contentsize: 1, fontSize: 24)
+                                                .padding(.leading, 24)
+                                            
+                                            
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.1)
+                                        
+                                        
+                                        // TODO: - Category 1
+                                        GeometryReader { firstGeo in
+                                            
+                                            Category1View()
+                                            
+                                            
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.5)
+                                        
+                                        
+                        // FIXME: - Category Text 2
+                                
+                                        GeometryReader { categoryOne in
+                                            
+                                            CategoryTextView(title: "Van/Pick up", contentsize: 1, fontSize: 24)
+                                            .padding(.leading, 24)
+                                            
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.1)
+                                        
+                                        
+                                        // TODO: - Category 2
+                                        GeometryReader { firstGeo in
+                                            
+                                            Category2View()
+                                            
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.5)
+                                        
+                                        
+                            // FIXME: - Category Text 3
+                                        
+                                                GeometryReader { categoryOne in
+                                                    
+                                                   CategoryTextView(title: "4x4/SUV", contentsize: 1, fontSize: 24)
+                                                    .padding(.leading, 24)
+                                                    
+                                                }
+                                                .frame(height: mainGeo.size.height * 0.1)
+                                        
+                                        // TODO: - Category 3
+                                        GeometryReader { firstGeo in
+                                            
+                                            Category3View()
+                                            
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.5)
+                                        
+                                        
+                            // FIXME: - Category Text 4
+                                        
+                                                GeometryReader { categoryOne in
+                                                    
+                                                    CategoryTextView(title: "OEM", contentsize: 1, fontSize: 24)
+                                                    .padding(.leading, 24)
+                                                    
+                                                }
+                                                .frame(height: mainGeo.size.height * 0.1)
+                                        
+                                        
+                                        // TODO: - Category 4
+                                        GeometryReader { firstGeo in
+                                            
+                                            Category4View()
+                                            
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.5)
+                                        
+                                        
+                                        GeometryReader { secondGeo in
+                                            
+                                            EmptyView()
+                                        }
+                                        .frame(height: mainGeo.size.height * 0.1)
                                         
                                     }
-                                    
-                                    Spacer()
-                                    
                                     
                                 }
-                                
                             }
-                            .frame(height: mainGeo.size.height * 0.1)
-                            
-                    // FIXME: - Category Text 1
-                            
-                            GeometryReader { categoryOne in
-                                
-                                
-                                CategoryTextView(title: "Passenger Car", contentsize: 1, fontSize: 24)
-                                    .padding(.leading, 24)
-                                
-                                
-                            }
-                            .frame(height: mainGeo.size.height * 0.1)
-                            
-                            
-                            // TODO: - Category 1
-                            GeometryReader { firstGeo in
-                                
-                                Category1View()
-                                
-                                
-                            }
-                            .frame(height: mainGeo.size.height * 0.5)
-                            
-                            
-            // FIXME: - Category Text 2
-                    
-                            GeometryReader { categoryOne in
-                                
-                                CategoryTextView(title: "Van/Pick up", contentsize: 1, fontSize: 24)
-                                .padding(.leading, 24)
-                                
-                            }
-                            .frame(height: mainGeo.size.height * 0.1)
-                            
-                            
-                            // TODO: - Category 2
-                            GeometryReader { firstGeo in
-                                
-                                Category2View()
-                                
-                            }
-                            .frame(height: mainGeo.size.height * 0.5)
-                            
-                            
-                // FIXME: - Category Text 3
-                            
-                                    GeometryReader { categoryOne in
-                                        
-                                       CategoryTextView(title: "4x4/SUV", contentsize: 1, fontSize: 24)
-                                        .padding(.leading, 24)
-                                        
-                                    }
-                                    .frame(height: mainGeo.size.height * 0.1)
-                            
-                            // TODO: - Category 3
-                            GeometryReader { firstGeo in
-                                
-                                Category3View()
-                                
-                            }
-                            .frame(height: mainGeo.size.height * 0.5)
-                            
-                            
-                // FIXME: - Category Text 4
-                            
-                                    GeometryReader { categoryOne in
-                                        
-                                        CategoryTextView(title: "OEM", contentsize: 1, fontSize: 24)
-                                        .padding(.leading, 24)
-                                        
-                                    }
-                                    .frame(height: mainGeo.size.height * 0.1)
-                            
-                            
-                            
-                            
-                            // TODO: - Category 4
-                            GeometryReader { firstGeo in
-                                
-                                Category4View()
-                                
-                            }
-                            .frame(height: mainGeo.size.height * 0.5)
-                            
-                            
-                            
-                            
-                            GeometryReader { secondGeo in
-                                
-                                EmptyView()
-                            }
-                            .frame(height: mainGeo.size.height * 0.1)
-                            
-                        }
-                        
                         
                     }
+                    .frame(minHeight: mainGeo.size.height * 0.9)
+
                     
+                    GeometryReader { secondGeo in
+                        
+                        EmptyView()
+                    }
+                    .frame(height: mainGeo.size.height * 0.1)
+                    .background(Color(red: 255/255, green: 222/255, blue: 0/255))
                     
                     
                 }
                 
                 
-            
-            
-            
+            }   // Finish Geometry
         }
+        
+        // Finish  ZStack
+        
+        
     }
 }
 
